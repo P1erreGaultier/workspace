0ae58489e7851032ab7dfb3a8adcab44149bf49c
P1erreGaultier <pit.gaultier@free.fr>
2015-10-13 22:41:47 +0200

    WHOLETHEDOGSOUT

--- SemanticWeb/src/TP2/Main.java
+++ SemanticWeb/src/TP2/Main.java
@@ -1,8 +1,6 @@
 package TP2;
 
-import java.io.IOException;
 
-import org.apache.jena.query.Query;
 import org.apache.jena.query.ResultSet;
 
 public class Main {
@@ -13,18 +11,52 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		SparQLQuery SPQR = new SparQLQuery();
-		SPQR.readData("dataset.rdf");
-		/*SPQR.getModel().write(System.out,"Turtle");*/
-		ResultSet R1=null;
-		Query Req1=null;
-		try {
-			Req1 = SPQR.readQuery("rq1.sparql");
-			
-		} catch (IOException e) {
-			System.out.println(e.getMessage());
-		}
-		R1 =SPQR.doQuery(Req1);
-		SPQR.res(R1);
+
+        ResultSet res = null;
+
+        // on charge le graphe par défaut à partir d'un modèle RDF/XML
+        SPQR.readDataDefaultGraph("peinture.rdfs");
+        
+        /* Requete 1 */
+        System.out.println(" Requete 1 ");
+		res = SPQR.doQuerySelect("requete1.rq");
+        SPQR.afficherRes(res);
+        
+        /* Requete 2 */
+        System.out.println(" Requete 2 ");
+		res = SPQR.doQuerySelect("requete2.rq");
+        SPQR.afficherRes(res);
+        
+        /* Requete 3*/
+        System.out.println(" Requete 3 ");
+		SPQR.doQueryUpdate("requete3.rq");
+
+        /* Requete 4-1 */        
+        System.out.println(" Requete 4-1 ");
+		res = SPQR.doQuerySelect("requete4-1.rq");
+        SPQR.afficherRes(res);
+
+        /* Requete 4-2 */
+        System.out.println(" Requete 4-2 ");
+		res = SPQR.doQuerySelect("requete4-2.rq");
+        SPQR.afficherRes(res);
+        
+        
+        /* Question 5 */
+        SPQR.enableRDFS(null);
+        
+        /* Question 6 */
+        
+        /* Requete 6-1 */        
+        System.out.println(" Requete 6-1 ");
+		res = SPQR.doQuerySelect("requete4-1.rq");
+        SPQR.afficherRes(res);
+        
+        /* Requete 6-2 */
+        System.out.println(" Requete 6-2 ");
+		res = SPQR.doQuerySelect("requete4-2.rq");
+        SPQR.afficherRes(res);
+        
 	}
 
 }
--- SemanticWeb/src/TP2/SparQLQuery.java
+++ SemanticWeb/src/TP2/SparQLQuery.java
@@ -8,26 +8,35 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
-import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.InfModel;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.sparql.core.DatasetImpl;
+import org.apache.jena.update.UpdateExecutionFactory;
+import org.apache.jena.update.UpdateFactory;
+import org.apache.jena.update.UpdateProcessor;
+import org.apache.jena.update.UpdateRequest;
 import org.apache.jena.util.FileManager;
 
 public class SparQLQuery {
 
-	private Model model ;
+	private Dataset dataset;
 	
-
+	/*
+	 *Constructeur
+	 */
 	public SparQLQuery() {
-		this.model = ModelFactory.createDefaultModel();
+		this.dataset = new DatasetImpl(ModelFactory.createDefaultModel());
 	}
 	
-	
-	public org.apache.jena.query.Query readQuery(String fichier) throws IOException{
+	/* Metode qui lie une fichier contenant une requete sparql et qui renvoie une String */
+	public String readQuery(String fichier) throws IOException{
         List<String> lines = null;
         String query = "";
         try {
@@ -36,50 +45,88 @@ public class SparQLQuery {
             e.printStackTrace();
         }
         for(String l: lines) {
-            query += " " + l;
+        	
+            query = query + " " + l;
         }
-		
-			return QueryFactory.create(query);
+			return query;
 	}
 
-	
-	public void readData(String input){
+	/* methode qui lit un fichier rdf, et qui construit le graphe par default correspondant */
+	public void readDataDefaultGraph(String input){
 	       InputStream in = FileManager.get().open( input );
 	        if (in == null) {
 	            throw new IllegalArgumentException( "File: " + input + " not found");
 	        }
-	        this.model.read(in, "");
+	        this.dataset.getDefaultModel().read(in, null);
+	}
+	
+	/* methode qui lit un fichier rdf, et qui construit le graphe nommé correspondant */
+	public void readDataNamedGraph(String uri,String input){
+	       InputStream in = FileManager.get().open( input );
+	        if (in == null) {
+	            throw new IllegalArgumentException( "File: " + input + " not found");
+	        }
+	        this.dataset.addNamedModel(uri, ModelFactory.createDefaultModel().read(in, null));
 	}
 
-	public ResultSet doQuery(org.apache.jena.query.Query Q){
-
+	/* methode qui execute une requete sparql de type select sur le dataset de cette classe */
+	public ResultSet doQuerySelect(String sQ){
+		Query Q = null;
 		ResultSet results=null;
-		        try {
-		        	QueryExecution qexec= QueryExecutionFactory.create(Q, model);
-		        	results = qexec.execSelect() ;
-		        } catch(Exception e) {
-		            System.err.println(e.getMessage());
-		        }
-
+		/*on crée la requette a partir du fichier */
+		try {
+			Q = QueryFactory.create(this.readQuery(sQ));
+		} catch (IOException e1) {
+			System.out.println(e1.getMessage());
+		}
+		/*Execution*/
+		try {
+		     QueryExecution qexec= QueryExecutionFactory.create(Q, dataset);
+		     results = qexec.execSelect() ;
+		} catch(Exception e) {
+		    System.err.println(e.getMessage());
+		}
 		return results;
 	}
 
-    public void res(ResultSet results) {
+	/* methode qui execute une requete sparql de type update sur le dataset de cette classe */
+    public void doQueryUpdate(String sQ) {
+    	UpdateRequest Q = null;
+    	/*on crée la requette a partir du fichier */
+		try {
+			Q = UpdateFactory.create(this.readQuery(sQ));
+		} catch (IOException e1) {
+			System.out.println(e1.getMessage());
+		}
+		/*Execution*/
+        try {
+            UpdateProcessor qexec = UpdateExecutionFactory.create(Q, this.dataset);
+            qexec.execute();
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+	/* methode qui active l'inference, soit sur le graphe par default, soit sur le graphe nommé en parametre */
+    public void enableRDFS(String uri) {
+        InfModel new_model = null;
+        if(uri == null) {
+            new_model = ModelFactory.createRDFSModel(this.dataset.getDefaultModel());
+            this.dataset.setDefaultModel(new_model);
+        } else { 
+            new_model = ModelFactory.createRDFSModel(this.dataset.getNamedModel(uri));
+            this.dataset.replaceNamedModel(uri, new_model);
+        }
+    }    
+    
+    /* methode qui affiche les resultat d'une requette dans le resultset passé en parametre*/
+    public void afficherRes(ResultSet results) {
         QuerySolution soln = null;
-
         for ( ; results.hasNext() ; ) {
             soln = results.nextSolution() ;
             System.out.println(soln.toString());
         }
     }
 
-	public Model getModel() {
-		return model;
-	}
-
-	public void setModel(Model model) {
-		this.model = model;
-	}
-
-
+    
+    
 }
